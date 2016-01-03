211d1508370e394f6d224d96c8587046c34ec096
Stephen Delaney <swdelaney2@gmail.com>
2016-01-03 16:59:05 -0600

    homework

--- scripts/bootflixAjax.js
+++ scripts/bootflixAjax.js
@@ -20,6 +20,27 @@ app.getMovieById = function getMovieById(id) {
   // 4. you call render() on the view
   // 5. your render() should append the `$el` to the DOM
 
+
+  var ajaxArgument = {
+    type: 'get',
+    url: 'http://www.omdbapi.com/?i=' + id + '&plot=full&r=json',
+    dataType: 'json',
+    success: function(data) {
+        console.log("success");
+        console.log(data);
+        var movie = new app.MovieModel(data);
+        var view = new app.MovieView(movie);
+        view.render();
+
+    },
+    error: function(error) {
+        console.log("error")
+        console.log(error);
+    }
+};
+// make the ajax call
+$.ajax(ajaxArgument);
+
 }
 
 /**
@@ -41,6 +62,26 @@ app.getMovieByTitle = function getMovieByTitle(title) {
   // 4. you call render() on the view
   // 5. your render() should append the `$el` to the DOM
 
+  var ajaxArgument = {
+    type: 'get',
+    url: 'http://www.omdbapi.com/?t=' + title + '&plot=full&r=json',
+    dataType: 'json',
+    success: function(data) {
+        console.log("success");
+        console.log(data);
+        var movie = new app.MovieModel(data);
+        var view = new app.MovieView(movie);
+        view.render();
+
+    },
+    error: function(error) {
+        console.log("error")
+        console.log(error);
+    }
+};
+// make the ajax call
+$.ajax(ajaxArgument);
+
 
 }
 
@@ -55,6 +96,13 @@ app.MovieModel = function MovieModel(options) {
   // id, title, rating, director, plot, year, genre should all be in the `options` object
   // store all the information in the model
 
+  this.id = options["imdbID"];
+  this.title = options["Title"];
+  this.rating = options["Rated"];
+  this.director = options["Director"];
+  this.plot = options["Plot"];
+  this.year = options["Year"];
+  this.genre = options["Genre"]
 }
 
 /**
@@ -67,10 +115,32 @@ app.MovieView = function MovieView(options) {
   // options should contain the `model` for which the view is using
 
   // 1. create a view
-  // 2. create a render() method
-  // 3. render() should a div with a class of '.movie' via string concatenation
-  //    you will want to add the id, title, rating, director, plot, year,
-  //    and genre. See design/movielayout.html
-  // 4. finally, render() will $(selector).append() each new '.movie' to "#movie-listing".
+    var view = {
+      id: options.id,
+      title: options.title,
+      rating: options.rating,
+      director: options.director,
+      plot: options.plot,
+      year: options.year,
+      genre: options.genre
+    };
+    // 2. create a render() method
+    this.render = function() {
+      var movie = document.createElement("div");
+      var html = "";
+      // 3. render() should a div with a class of '.movie' via string concatenation
+      //    you will want to add the id, title, rating, director, plot, year,
+      //    and genre. See design/movielayout.html
+      movie.className = "movie";
+      html = "<li>Title: " + view.title + "</li><li>Rated: " + view.rating + "</li><li>Director: " + view.director + "</li><li>Plot: " + view.plot + "</li><li>Released: " + view.year + "</li><li>Genre: " + view.genre + "</li>";
+      if (view.title == undefined){
+        html = "Movie not found!"
+      };
+      // 4. finally, render() will $(selector).append() each new '.movie' to "#movie-listing".
+      movie.innerHTML = html;
+      var movieList = document.getElementById("movie-listing");
+      movieList.appendChild(movie);
+    }
 
-}
+
+  }
